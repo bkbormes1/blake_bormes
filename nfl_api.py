be689b36bdd408e57cae4cfa0667ca1e7009b3c3
alexdrossos <jupyter@python-20210825-172026.c.alien-house-324100.internal>
2021-11-18 20:09:49 +0000

    altered nfl_api.py to work with our API

--- nfl_api.py
+++ nfl_api.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
 import json
+import requests
 from kafka import KafkaProducer
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 producer = KafkaProducer(bootstrap_servers='kafka:29092')
 
 
 def log_to_kafka(topic, event):
-    event.update(request.headers)
     producer.send(topic, json.dumps(event).encode())
-
 
 @app.route("/")
 def default_response():
-    # need to input GET API to pull games JSON
     default_event = {'event_type': 'default'}
-    log_to_kafka('games', default_event)
-    return "Game logged!\n"
+    log_to_kafka('events', default_event)
+    return "This is the default response!\n"
 
-
-# @app.route("/purchase_a_sword")
-#def purchase_a_sword():
-#    purchase_sword_event = {'event_type': 'purchase_sword'}
-#    log_to_kafka('events', purchase_sword_event)
-#    return "Sword Purchased!\n"
+@app.route("/get_game_data")
+def get_game_data():
+    my_headers = {'Ocp-Apim-Subscription-Key' : 'c1a67aa2c0454422923a280f26415860'}
+    game_update = requests.get('https://api.sportsdata.io/v3/nfl/scores/json/ScoresByDate/2021-OCT-31', headers=my_headers)
+    log_to_kafka('games', game_update.json())
+    return "Game Update!\n"
